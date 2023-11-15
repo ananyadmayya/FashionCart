6dfef0cd83edf45a01af55dcf8a291b3e74ece72
ananyadmayya <ananya.19cs009@sode-edu.in>
2023-11-15 19:47:48 +0530

    third commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Container} from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import {Outlet} from 'react-router-dom';
+
 
 const App=() => {
   return (
@@ -9,7 +11,8 @@ const App=() => {
       <Header />
       <main className='py-3'>
         <Container>
-           <h1>Welcome to Fashion Cart</h1>
+        <Outlet />
+
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer=() => {
             <Container>
                 <Row>
                     <Col className='text-center py-3'>
-                        <p>FashionCart &copy; {currentYear}</p>
+                        <p>ProShop &copy; {currentYear}</p>
                     </Col>    
                  </Row>
             </Container>
