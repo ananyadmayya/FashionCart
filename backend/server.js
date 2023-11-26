fc13b908d7ec8dfac0c45820b26fb41e1ddf6ff2
ananyadmayya <ananya.19cs009@sode-edu.in>
2023-11-26 11:05:50 +0530

    sixth commit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js';
-
+import userRoutes from './routes/userRoutes.js'
 
 connectDB();
 
@@ -12,11 +13,19 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//cookie parser middleware
+app.use(cookieParser());
+
 app.get('/',(req, res) => {
     res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
