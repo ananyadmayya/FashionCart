333332e253180ba263a82dc231711884f6f95243
ananyadmayya <ananya.19cs009@sode-edu.in>
2023-11-29 10:33:34 +0530

    seventh

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -24,18 +24,28 @@ const cartSlice = createSlice({
             return updateCart(state);
         },
 
-        saveShippingAddress: (state, action) => {
-            state.shippingAddress = action.payload;
-            return updateCart(state);
-         },
-
-
         removeFromCart:  (state,action) => {
             state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
 
             return updateCart(state);
         },
+
+        saveShippingAddress: (state, action) => {
+            state.shippingAddress = action.payload;
+            return updateCart(state);
+         },
+
+         savePaymentMethod: (state, action) => {
+            state.paymentMethod = action.payload;
+            return updateCart(state);
+         },
+         clearCartItems: (state, action) => {
+            state.cartItems = [];
+            return updateCart(state);
+         },
+
+
     },
 });
-export const {addToCart , removeFromCart, saveShippingAddress} = cartSlice.actions;
+export const {addToCart , removeFromCart, saveShippingAddress,savePaymentMethod,clearCartItems} = cartSlice.actions;
 export default cartSlice.reducer;
